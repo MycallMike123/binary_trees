1c2151f66485c54b91a3409b136288dfdb80748f
Michael Mwangi <michaelmichire2016@gmail.com>
2024-02-29 03:24:12 -0800

    The main header file for the project

--- binary_trees.h
+++ binary_trees.h
@@ -24,9 +24,6 @@ struct binary_tree_s
 };
 
 typedef struct binary_tree_s binary_tree_t;
-typedef struct binary_tree_s bst_t;
-typedef struct binary_tree_s avl_t;
-typedef struct binary_tree_s heap_t;
 
 /**
  * struct levelorder_queue_s - Level order traversal queue.
